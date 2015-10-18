eb5207facdb6b64509510491571c7e00f74b1d5a
JUASP-G73-Android <JUASP-G73-Android@192.168.254.131>
2015-10-18 00:07:01 -0400

    Gestion de la requete et de la reponse pour un bet

--- Serveur/server/MessageHandler.java
+++ Serveur/server/MessageHandler.java
@@ -10,9 +10,11 @@ import org.apache.log4j.Logger;
 
 import dataManagement.ListeDesMatchs;
 import dataObject.Bet;
+import dataObject.BetRespond;
 import dataObject.ListMatchName;
 import dataObject.Match;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import protocole.Message;
@@ -75,20 +77,72 @@ public class MessageHandler implements Runnable {
 				reply.setValue(matchDetail);			
 				break;
                         case betInfo:
-                                Object[] args = request.getArgument();
-                                int betMatchID = (int) args [0];
-                                String betID = (String) args[1];
-                                logger.info("detail received with params : "+ String.valueOf(betMatchID + " and the betID: " + betID.toString() ));                                
+                            
+                                //We fetch the arguments
+                                Object[] args = request.getArgument();                
+                                int betMatchID = (int) args [0]; // The matchID of the request                            
+                                String betID = (String) args[1]; // The betID of the request
+                                
+                                logger.info("detail received with params : "+ String.valueOf(betMatchID + " and the betID: " + betID.toString() ));  
+                                
+                                //Respond object
+                                BetRespond betRespond = null;
+                                
                                 try {
-                                    float totalBettingAmount = BetHandler.getTotalBettingAmount(betMatchID);
+                                    //We get the total betting amount for the current match
+                                    float matchTotalBettingAmount = BetHandler.getTotalBettingAmount(betMatchID);
+                                    
+                                    logger.info("Total betting amount for the match #"+ String.valueOf(betMatchID + " is : " + String.valueOf(matchTotalBettingAmount) + "$" ));
+                                    
+                                    //We retreive the data of the Match
                                     Match betMatchDetail = data.getMatch(betMatchID);
-                                    Hashtable<String, Bet> winnerTable = BetHandler.getWinnerTable(betMatchDetail);
-                                    if(winnerTable.containsKey(betID)){
-                                        winnerTable.get(betID);                                        
-                                        reply.setValue("Congratulation you win: " + betID + "did not win!");
+                                    
+                                    if(betMatchDetail.getWinner() != null){
+
+                                        //We get the winnerTable that contains all the Bet object that won for that match.
+                                        Hashtable<String, Bet> winnerTable = BetHandler.getWinnerTable(betMatchDetail);
+
+                                        //If the current betID (from the request) is inside the winnerTable we proceed
+                                        if(winnerTable.containsKey(betID)){
+
+                                            //We set currentBet to the resquested bet with the betID 
+                                            Bet currentBet = winnerTable.get(betID);
+
+                                            logger.info("The current bet amount for the match #"+ String.valueOf(betMatchID + " is : " + String.valueOf(currentBet) + "$" ));
+
+                                            float currentBetAmount = currentBet.getBetAmount(); //contains the resquested bet dollar amount 
+
+                                            float totalWinningBetAmount = 0; //contains the sum of all the bet
+
+                                            //This iterator will go through the winnerTable 
+                                            Iterator<Map.Entry<String, Bet>> it = winnerTable.entrySet().iterator();
+
+                                            while (it.hasNext()) {
+                                              //We get the current entry of the winnerTable  
+                                              Map.Entry<String, Bet> entry = it.next();
+
+                                              //We get the value of that entry
+                                              Bet b = entry.getValue();
+
+                                              //We sum the Bet value to the existing totalWinningBetAmount
+                                              totalWinningBetAmount += b.getBetAmount();
+                                            }
+
+                                            logger.info("The total of the winning bet made for the match #"+ String.valueOf(betMatchID + " is : " + String.valueOf(totalWinningBetAmount) + "$" ));
+
+                                            float wonAmount = (float)( (currentBetAmount / totalWinningBetAmount) * (matchTotalBettingAmount * 0.75) );
+
+                                            betRespond = new BetRespond(betID, betMatchID, 1, currentBetAmount, wonAmount ); // won the bet
+                                            reply.setValue(betRespond);
+                                        }
+                                        else{
+                                            betRespond = new BetRespond(betID, betMatchID, 0 ); // Did not win the bet
+                                            reply.setValue(betRespond);
+                                        }
                                     }
                                     else{
-                                        reply.setValue("The bet: " + betID + "did not win!");
+                                        betRespond = new BetRespond(betID, betMatchID, 2 ); // Match isn't finish yet
+                                        reply.setValue(betRespond);
                                     }
                                 } catch (IOException ex) {
                                         java.util.logging.Logger.getLogger(MessageHandler.class.getName()).log(Level.SEVERE, null, ex);
